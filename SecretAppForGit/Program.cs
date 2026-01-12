ea508342f670029268f84bcface0cc1ac7715e1d
Elis Gillberg <elis.r.gillberg@gmail.com>
2026-01-12 13:27:12 +0100

    Add project files.

---
+++ SecretAppForGit/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SecretAppForGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hell, World!");
+        }
+    }
+}
